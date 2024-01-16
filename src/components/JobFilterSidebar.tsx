7244355f7e03c85410b7af7d97ace07f03567acd
Josy-coder <josekecha@keemail.me>
2024-01-16 18:44:45 +0200

    Add job type filter and distinct location dropdown

--- src/components/JobFilterSidebar.tsx
+++ src/components/JobFilterSidebar.tsx
@@ -1,12 +1,23 @@
+import { jobTypes } from "@/lib/job-types";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import Select from "./ui/select";
+import prisma from "@/lib/prisma";
 
 async function filterJobs(formData: FormData) {
     "use server";
 }
 
-export default function JobFilterSidebar() {
+export default async function JobFilterSidebar() {
+
+    const distinctLocation = (await prisma.job.findMany({
+        where: {approved: true},
+        select: {location: true},
+        distinct: ["location"]
+    }).then((locations) =>
+        locations.map(({location}) => location).filter(Boolean),
+        )) as string[]
+
     return (
         <aside className='md:w-[260px] p-4 sticky top-0 h-fit bg-background border rounded-lg'>
             <form action={filterJobs}>
@@ -15,10 +26,30 @@ export default function JobFilterSidebar() {
                         <Label htmlFor="query">Search</Label>
                         <Input id="query" name="query" placeholder="Title, company, etc..." />
                     </div>
+
+                    <div className="flex flex-col gap-2">
+                        <Label htmlFor="type">Type</Label>
+                        <Select id="type" name="type" defaultValue="">
+                            <option value="" >All locations</option>
+                                {jobTypes.map((type) => (
+                                    <option key={type} value={type}>
+                                        {type}
+                                    </option>
+
+                                ))}
+                        </Select>
+                    </div>
+
                     <div className="flex flex-col gap-2">
                         <Label htmlFor="location">Location</Label>
-                        <Select id="location" name="location">
+                        <Select id="location" name="location" defaultValue="">
+                            <option value="" >All locations</option>
+                            {distinctLocation.map((location) => (
+                                <option key={location} value={location}>
+                                    {location}
+                                </option>
 
+                            ))}
                         </Select>
 
                     </div>
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -12,6 +12,7 @@ export default forwardRef<
                     "h-10 border rounded-md appearance-none truncate bg-background border-input py-2 pl-3 pr-8 text-sm ring-offset-background focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
                     className
                 )}
+                ref={ref}
                 {...props}
             />
             <ChevronDown className="absolute right-3 top-3 h-4 w-4 opacity-50" />
